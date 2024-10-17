2cefe51f25c4b638ba73e90e1127e16f8298f4ff
Mauro Sérgio <maurosergiocantuaria@gmail.com>
2024-10-17 11:11:48 -0300

    test

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import Header from '../components/Header';
-import Contato from '../components/contato';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from '@fortawesome/free-solid-svg-icons';
-import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
-import { faGithub, faLinkedin, faReact, faJsSquare, faHtml5, faCss3Alt, faNodeJs, faPython } from '@fortawesome/free-brands-svg-icons';
 import Slider from "react-slick";
 import ListaCard from '../components/ListaCard';
-import { faDribbble } from '@fortawesome/free-brands-svg-icons';
 import MySVG from '../assets/svg/undraw_mobile_messages_re_yx8w.svg'
+import emailjs from 'emailjs-com'; // Importando o SDK
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin, faReact, faJsSquare, faHtml5, faCss3Alt, faNodeJs, faPython } from '@fortawesome/free-brands-svg-icons';
+import { faDribbble } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
 
 export default function Home() {
 
@@ -47,6 +47,20 @@ export default function Home() {
     // Adicione mais projetos conforme necessário
   ];
 
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_dn3ylur', 'template_adx3pu5', e.target, 'VV6fWx3od9TlKEMCo')
+      .then((result) => {
+        console.log(result.text);
+        alert("Mensagem enviada com sucesso!");
+      }, (error) => {
+        console.log(error.text);
+        alert("Erro ao enviar mensagem. Tente novamente mais tarde.");
+      });
+  };
+
+
   return (
     <div className="min-h-screen bg-gray-900 text-white overflow-x-hidden">
       <Header />
@@ -66,7 +80,7 @@ export default function Home() {
           </div>
         </div>
         <div className="w-full md:w-1/2 flex justify-center items-center">
-          <img src={MySVG} alt="Ilustração" className="w- h-auto" /> {/* Usando o SVG como imagem */}
+          <img src={MySVG} alt="Ilustração" className="w-[70%] h-auto" /> {/* Usando o SVG como imagem */}
         </div>
       </div>
 
@@ -74,8 +88,8 @@ export default function Home() {
       <div id="Sobre" className="w-full flex md:flex-row-reverse flex-col justify-center align-middle bg-gray-900 p-8">
         <div className="w-full md:w-2/3 p-4 md:p-8">
           <h1 className="pb-4 text-xl font-semibold">SOBRE</h1>
-          <p>Conheça um pouco sobre mim</p>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry...</p>
+          <p className="py-2">Conheça um pouco sobre mim.</p>
+          <p>Sou um desenvolvedor full stack com experiência em React, Typescript, Tailwind, PostgreeSQL e NextUI, especializado na criação de interfaces modernas e funcionais. Com formação em Engenharia de Software e prática em desenvolvimento de projetos pessoais e sistemas de gerenciamento, estou sempre aprendendo novas tecnologias para expandir minhas capacidades. Busco aplicar minhas habilidades para resolver problemas complexos e entregar soluções inovadoras.</p>
         </div>
         <div className="flex w-full md:w-1/3 flex-col items-center space-y-4">
           <FontAwesomeIcon icon={faUser} className="text-cyan-400 w-16 h-16 md:w-24 md:h-24" />
@@ -111,7 +125,101 @@ export default function Home() {
       </div>
 
       {/* Contato */}
-      <Contato />
+      <div id="Contato" className="bg-gray-800 py-12 px-6">
+        <div className="max-w-7xl mx-auto text-center">
+          <h2 className="font-bold text-cyan-400 mb-4 text-xl md:text-3xl">Entre em Contato</h2>
+          <p className="text-white mb-8 text-sm md:text-base">
+            Estou sempre aberto a novas oportunidades e colaborações. Se você quiser discutir um projeto, compartilhar ideias, fique à vontade para entrar em contato!
+          </p>
+
+          <div className="flex flex-col items-center space-y-4 mb-8">
+            <div className="flex items-center space-x-3">
+              <FontAwesomeIcon icon={faEnvelope} size="2x" className="text-cyan-400" />
+              <a
+                href="mailto:maurosergiocantuaria@gmail.com?subject=Contato&body=Olá Mauro,"
+                className="text-cyan-400 hover:text-cyan-600 text-sm md:text-base"
+              >
+                maurosergiocantuaria@gmail.com
+              </a>
+            </div>
+
+            <div className="flex items-center space-x-3">
+              <FontAwesomeIcon icon={faPhone} size="2x" className="text-cyan-400" />
+              <a
+                href="https://wa.me/5561999594718"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-cyan-400 hover:text-cyan-600 text-sm md:text-base"
+              >
+                +55 61 99959-4718
+              </a>
+            </div>
+
+            <div className="flex items-center space-x-3">
+              <FontAwesomeIcon icon={faGithub} size="2x" className="text-cyan-400" />
+              <a
+                href="https://github.com/MauroSergio10?tab=repositories"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-cyan-400 hover:text-cyan-600 text-sm md:text-base"
+              >
+                github.com/MauroSergio10
+              </a>
+            </div>
+
+            <div className="flex items-center space-x-3">
+              <FontAwesomeIcon icon={faLinkedin} size="2x" className="text-cyan-400" />
+              <a
+                href="https://www.linkedin.com/in/mauro-sergio-15a60a19a/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-cyan-400 hover:text-cyan-600 text-sm md:text-base"
+              >
+                linkedin.com/in/mauro-sergio-15a60a19a
+              </a>
+            </div>
+          </div>
+
+          <form onSubmit={sendEmail} className="w-full max-w-lg mx-auto space-y-4">
+            <div className="flex flex-col">
+              <label className="text-white text-left" htmlFor="name">Nome</label>
+              <input
+                id="name"
+                name="name" // Certifique-se de que o nome do campo corresponde ao esperado no template do EmailJS
+                type="text"
+                className="p-2 rounded-md bg-gray-900 text-white border border-cyan-400 focus:ring-2 focus:ring-cyan-600 text-sm"
+                placeholder="Seu nome"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label className="text-white text-left" htmlFor="email">Email</label>
+              <input
+                id="email"
+                name="email" // Certifique-se de que o nome do campo corresponde ao esperado no template do EmailJS
+                type="email"
+                className="p-2 rounded-md bg-gray-900 text-white border border-cyan-400 focus:ring-2 focus:ring-cyan-600 text-sm"
+                placeholder="Seu email"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label className="text-white text-left" htmlFor="message">Mensagem</label>
+              <textarea
+                id="message"
+                name="message" // Certifique-se de que o nome do campo corresponde ao esperado no template do EmailJS
+                rows="4"
+                className="p-2 rounded-md bg-gray-900 text-white border border-cyan-400 focus:ring-2 focus:ring-cyan-600"
+                placeholder="Escreva sua mensagem"
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full py-2 px-4 rounded-md bg-cyan-400 text-gray-900 font-bold hover:bg-cyan-600"
+            >
+              Enviar Mensagem
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
